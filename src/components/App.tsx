9c7d6a1f9d6827c099a0577aaa4f7403a36975c6
Andrey78945 <platonovaudit@yandex.ru>
2023-05-14 19:38:29 +0500

    feat: add async

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Counter } from "./Counter/Counter";
 import "../index.scss";
+import { Link, Route, Routes } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 function App() {
   return (
     <div className="app">
+      <Link to="/about">About</Link>
+      <Link to="/">Home</Link>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
       <Counter />
     </div>
   );
