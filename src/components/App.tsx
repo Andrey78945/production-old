ce0eb59d91436cb53f4acc00d7d5acb2f0752b36
Andrey78945 <platonovaudit@yandex.ru>
2023-05-16 09:09:15 +0500

    feat: add themes and Context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,40 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useContext, useState } from "react";
 import { Counter } from "./Counter/Counter";
-import "../index.scss";
+import "../styles/index.scss";
 import { Link, Route, Routes } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "../pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "../pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from "../theme/themeContext";
 
 function App() {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  };
+
   return (
-    <div className="app">
-      <Link to="/about">About</Link>
-      <Link to="/">Home</Link>
+    <div className={"app " + theme}>
+      <nav>
+        <Link to="/" style={{ marginRight: "30px" }}>
+          Home
+        </Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <button
+        onClick={toggleTheme}
+        style={{ marginLeft: "auto", marginRight: "0" }}
+      >
+        Theme
+      </button>
       <Suspense fallback={<h2>Loading...</h2>}>
         <Routes>
           <Route path="/" element={<MainPageAsync />} />
           <Route path="/about" element={<AboutPageAsync />} />
         </Routes>
       </Suspense>
+
       <Counter />
     </div>
   );
