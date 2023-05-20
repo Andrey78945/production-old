1db96f0947ee2a7e46217283a08d31c0f31786d6
Andrey78945 <platonovaudit@yandex.ru>
2023-05-20 19:30:04 +0500

    feat: add styles to AppLink component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cls from "./Navbar.module.scss";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AppLink } from "shared/ui/AppLink/AppLink";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 
 interface NavbarProps {
   className?: string;
@@ -11,10 +11,16 @@ export const Navbar = ({ className }: NavbarProps) => {
   return (
     <header className={classNames(cls.Navbar, {}, [])}>
       <nav className={classNames(cls.header__nav, {}, [])}>
-        <AppLink to="/" style={{ marginRight: "30px" }}>
+        <AppLink
+          to="/"
+          style={{ marginRight: "30px" }}
+          theme={AppLinkTheme.SECONDARY}
+        >
           Home
         </AppLink>
-        <AppLink to="/about">About</AppLink>
+        <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
+          About
+        </AppLink>
       </nav>
     </header>
   );
