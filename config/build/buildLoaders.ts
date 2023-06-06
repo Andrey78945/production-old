1e595c42063d7670b6f031e23a9bdd73f915ad19
Andrey78945 <platonovaudit@yandex.ru>
2023-06-06 18:06:59 +0500

    feat: add storybook7

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,47 +1,26 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import webpack from "webpack";
-import { BuildOptions } from "./types/config";
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-  const cssLoader = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      // Creates `style` nodes from JS strings
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      // Translates CSS into CommonJS
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
-            localIdentName: isDev
-              ? "[path][name]__[local]--[hash:base64:8]"
-              : "[hash:base64:8]",
-            exportLocalsConvention: "camelCase",
-          },
-        },
-      },
-      // Compiles Sass to CSS
-      "sass-loader",
-    ],
-  };
+  const cssLoader = buildCssLoader(isDev);
   const typescriptLoader = {
     test: /\.tsx?$/,
-    use: "ts-loader",
+    use: 'ts-loader',
     exclude: /node_modules/,
   };
 
   const svgLoader = {
     test: /\.svg$/i,
     issuer: /\.[jt]sx?$/,
-    use: ["@svgr/webpack"],
+    use: ['@svgr/webpack'],
   };
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2)$/i,
     use: [
       {
-        loader: "file-loader",
+        loader: 'file-loader',
       },
     ],
   };
@@ -50,20 +29,20 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     test: /\.[jt]sx?$/,
     exclude: /node_modules/,
     use: {
-      loader: "babel-loader",
+      loader: 'babel-loader',
       options: {
         presets: [
-          ["@babel/preset-env", { targets: "defaults" }],
-          "@babel/preset-react",
+          ['@babel/preset-env', { targets: 'defaults' }],
+          '@babel/preset-react',
         ],
         plugins: [
           [
-            "i18next-extract",
+            'i18next-extract',
             {
-              locales: ["en", "ru"],
+              locales: ['en', 'ru'],
               keyAsDefaultValue: true, // false
               saveMissing: true,
-              outputPath: "public/locales/{{locale}}/{{ns}}.json", // t("key", {ns: "locale-file"})
+              outputPath: 'public/locales/{{locale}}/{{ns}}.json', // t("key", {ns: "locale-file"})
             },
           ],
         ],
---
+++ config/storybook/main.ts
@@ -0,0 +1,32 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+  stories: ['../../src/**/*.mdx', '../../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    // {
+    //   name: '@storybook/addon-styling',
+    //   options: {
+    //     cssModules: true,
+    //     sass: {
+    //       // Require your Sass preprocessor here
+    //       // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+    //       implementation: require('sass'),
+    //     },
+    //   },
+    // },
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  core: {
+    disableTelemetry: true,
+  },
+};
+export default config;
