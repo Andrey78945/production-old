ed80eb1fcb1fd4e3bcefc30535f195f0dd7363c0
Andrey78945 <platonovaudit@yandex.ru>
2023-06-23 08:43:42 +0500

    feat:add counter test, redux

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,25 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { componentRender } from 'shared/lib/componentRender/componentRender';
+import { Counter } from './Counter';
+
+describe('Counter', () => {
+  test('test render', () => {
+    componentRender(<Counter />, { initialState: { counter: { value: 10 } } });
+    expect(screen.getByTestId('value-title')).toBeInTheDocument();
+    expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+  });
+
+  test('test increment', () => {
+    componentRender(<Counter />, { initialState: { counter: { value: 10 } } });
+    expect(screen.getByTestId('btn-increment')).toBeInTheDocument();
+    fireEvent.click(screen.getByTestId('btn-increment'));
+    expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+  });
+
+  test('test decrement', () => {
+    componentRender(<Counter />, { initialState: { counter: { value: 10 } } });
+    expect(screen.getByTestId('btn-decrement')).toBeInTheDocument();
+    fireEvent.click(screen.getByTestId('btn-decrement'));
+    expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+  });
+});
